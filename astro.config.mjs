bdab40430c57c3255545754baca3363e529f0611
Keisuke Taniguchi <rememberthatk@icloud.com>
2023-04-23 19:06:32 +0900

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    analytics: true,
+    analytics: true
   }),
-  integrations: [react()],
-})
+  integrations: [react(), tailwind()]
+});
